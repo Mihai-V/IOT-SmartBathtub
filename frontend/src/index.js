bec8f91f7ae58a0eb47ec9fb758a38474b98bc6e
Haginus <hagiandrei.ah@gmail.com>
2021-05-21 17:56:02 +0300

    added env variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
+require('dotenv').config();
 
 ReactDOM.render(
   <React.StrictMode>
